9d3db5b234913be86f9a402846cb7141faec8e7f
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-05 16:42:33 +0200

    Add Correction_01 part 1

---
+++ CSharpWars/Army/Soldat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWars.Army
+{
+    internal class Soldat
+    {
+        #region fields
+        private int _vie = 100;
+        #endregion
+
+        #region props
+        public int Vie
+        {
+            get { return _vie; }
+            private set { 
+                if (value < 0 ) { _vie = 0; }
+                else _vie = value;
+            }
+        }
+
+        public int VieMax { get; private set; } = 100;
+
+        public string Nom { get; set; }
+        public int Puissance { get; set; }
+        public int Defense { get; set; }
+        #endregion
+
+        #region methods
+
+        #endregion
+    }
+}
---
+++ CSharpWars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
