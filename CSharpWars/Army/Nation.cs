7fdc86c7f88ebab69c1be6efda42344bc8436502
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-06 16:46:20 +0200

    Add Correction Exo02 part1

---
+++ CSharpWars/Army/Nation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpWars.Army
+{
+    internal class Nation
+    {
+        #region Fields
+        private Dictionary<string, Soldat> _armee;
+        #endregion
+
+        #region Props
+        public string Nom { get; set; }
+        public int Credit { get; private set; } = 10_000;
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
