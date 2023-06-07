e9c2ef2636da601aa249144e1829546febc841d8
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-07 12:20:20 +0200

    Add Demo Surcharge opérateur Part1

---
+++ Demo-Operateur/Panier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Operateur
+{
+    internal class Panier
+    {
+        private readonly List<Fruit> _fruits = new List<Fruit>();
+
+        public void AddFruit(Fruit f)
+        {
+            _fruits.Add(f);
+        }
+
+        public static Panier operator +(Panier left, Panier right) { 
+            Panier result = new Panier();
+
+            foreach (Fruit f in left._fruits)
+            {
+                result.AddFruit(f);
+            }
+
+            foreach (Fruit f in right._fruits)
+            {
+                result.AddFruit(f);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Demo-Operateur/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo_Operateur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Panier p1 = new Panier();
+            p1.AddFruit(new Fruit() { Nom = "Pomme" });
+            p1.AddFruit(new Fruit() { Nom = "Pomme" });
+            p1.AddFruit(new Fruit() { Nom = "Pomme" });
+            p1.AddFruit(new Fruit() { Nom = "Orange" });
+
+            // p1 contient 3 Pommes et 1 Orange
+
+            Panier p2 = new Panier();
+            p2.AddFruit(new Fruit() { Nom = "Orange" });
+            p2.AddFruit(new Fruit() { Nom = "Fraise" });
+            p2.AddFruit(new Fruit() { Nom = "Orange" });
+
+            // p2 contient 2 Oranges et 1 Fraise
+
+            Panier resultatRecolte = p1 + p2;
+        }
+    }
+}
