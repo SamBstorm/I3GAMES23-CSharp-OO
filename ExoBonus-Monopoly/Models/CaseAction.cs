675c3c0a6fe9de19533a643472475e48dd8518ce
Samuel Legrain <samuel.legrain@bstorm.be>
2023-06-21 16:10:16 +0200

    Update Exo-Monopoly Part 02

---
+++ ExoBonus-Monopoly/Models/CaseAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoBonus_Monopoly.Models
+{
+    internal class CaseAction : Case
+    {
+        public CaseAction(string nomCase) : base(nomCase)
+        {
+        }
+
+        public void ExecuterAction(Joueur joueurActuel)
+        {
+            Console.WriteLine($"{joueurActuel.Nom} est sur la case {this.NomCase}!");
+        }
+    }
+}
---
+++ ExoBonus-Monopoly/Models/Jeu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoBonus_Monopoly.Models
+{
+    internal class Jeu
+    {
+        public List<Case> Plateau { get; private set; }
+        public List<Joueur> Joueurs { get; private set; }
+    }
+}
